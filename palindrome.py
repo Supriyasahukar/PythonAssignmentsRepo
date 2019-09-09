d21447c6d5540873e4d6e54d6a376f47996b7951
Supriyasahukar <55086974+Supriyasahukar@users.noreply.github.com>
2019-09-09 16:05:24 +0530

    #alternate method

--- palindrome.py
+++ palindrome.py
@@ -9,3 +9,12 @@ if list(str)==list(rev):
     print("palindrome")
 else:
     print("not palindrome")
+
+#alternate method
+'''
+input_string=input("enter any string:")
+if input_string[0: ]==input_string[ ::-1]:
+    print("string is Palindrome" )
+else:
+    print("string is not a Palindrome")
+    '''
